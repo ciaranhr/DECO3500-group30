c1a77386b3f19de186ec4b5b14e65347afe5d834
Ciaran Huang-Ryan <85781351+ciaranhr@users.noreply.github.com>
2024-10-21 12:36:10 +1000

    archive imageTracking

---
+++ Assets/Scripts/ImageTrackingOld.cs
@@ -0,0 +1,181 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ImageTracker : MonoBehaviour
+{
+    public GameObject museum;
+    private ARTrackedImageManager trackedImages;
+    public GameObject[] arPrefabs;
+    public GameObject infoPopup;
+    private GameObject instance;
+    private ARAnchorManager anchorManager;
+
+    List<GameObject> ARObjects = new List<GameObject>();
+
+
+    void Awake()
+    {
+        trackedImages = GetComponent<ARTrackedImageManager>();
+    }
+
+    void OnEnable()
+    {
+        trackedImages.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        trackedImages.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    private GameObject GetChild (GameObject parent, string name)
+    {
+        GameObject emptyObject = new GameObject("EmptyObject");
+        GameObject currentObject;
+        // Return the empty GameObject
+
+        for (int i = 0; i < parent.transform.childCount; i++) {
+            currentObject = parent.transform.GetChild(i).gameObject;
+            if (currentObject.name == name)
+            {
+                return currentObject;
+
+            }
+        }
+        return emptyObject;
+    }
+
+    private GameObject GetActiveChild(GameObject parent)
+    {
+        GameObject emptyObject = new GameObject("EmptyObject");
+        GameObject currentObject;
+        string[] ignore = { "Colours", "Composition", "Send" };
+
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            currentObject = parent.transform.GetChild(i).gameObject;
+            if (!ignore.Contains(currentObject.name))
+            {
+                if(currentObject.activeInHierarchy)
+                {
+                    Debug.Log("current active object: " + currentObject);
+                    return currentObject;
+                    
+                }
+
+            }
+        }
+        return emptyObject;
+    }
+
+
+    // Event Handler
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        //Create object based on image tracked
+        foreach (var trackedImage in eventArgs.added)
+        {
+            Debug.Log("Image Recognised");
+            foreach (var arPrefab in arPrefabs)
+            {
+                if (trackedImage.referenceImage.name == arPrefab.name)
+                {
+                    
+                    var newPrefab = Instantiate(arPrefab, trackedImage.transform);
+                    ARObjects.Add(newPrefab);
+                    GetChild(newPrefab, "ColourInfo").SetActive(false);
+                    GetChild(newPrefab, "CompositionExtra1").SetActive(false);
+                    GetChild(newPrefab, "SendBall").SetActive(false);
+                    /*
+                    newPrefab = Instantiate(GetChild(arPrefab, "Send"), trackedImage.transform);
+                    ARObjects.Add(newPrefab);
+                    newPrefab = Instantiate(GetChild(arPrefab, "Composition"), trackedImage.transform);
+                    ARObjects.Add(newPrefab);
+                    newPrefab = Instantiate(GetChild(arPrefab, "Default"), trackedImage.transform);
+                    ARObjects.Add(newPrefab);
+                   */
+                }
+            }
+        }
+
+        //Update tracking position
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            foreach (var gameObject in ARObjects)
+            {
+                if (gameObject.name == trackedImage.name)
+                {
+                    gameObject.SetActive(trackedImage.trackingState == TrackingState.Tracking);
+                }
+            }
+        } 
+    }
+
+    void Update()
+    {
+        GameObject activeChild;
+        GameObject hitTargetParent;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log(hit.transform.name + " : " + hit.transform.tag);
+                hitTargetParent = hit.transform.parent.gameObject;
+                activeChild = GetActiveChild(hitTargetParent);
+                if (hit.transform.tag == "Composition")
+                {
+                    if (activeChild.name != "default")
+                    {
+                        activeChild.SetActive(false);
+                        GetChild(hitTargetParent, "default").SetActive(true);
+                        
+                    }
+                    activeChild = GetActiveChild(hitTargetParent);
+                }
+
+                else if (hit.transform.tag == "Send")
+                {
+                    if (activeChild.name != "SendBall")
+                    {
+                        activeChild.SetActive(false);
+                        GetChild(hitTargetParent, "SendBall").SetActive(true);
+
+                    }
+                    activeChild = GetActiveChild(hitTargetParent);
+                }
+
+                else if (hit.transform.tag == "Colours")
+                {
+                    if (activeChild.name != "ColourInfo")
+                    {
+                        activeChild.SetActive(false);
+                        GetChild(hitTargetParent, "ColourInfo").SetActive(true);
+                        
+                    }
+                    activeChild = GetActiveChild(hitTargetParent);
+                } else if (hit.transform.tag == "Floor")
+                {
+                    GameObject newObject = Instantiate(museum, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+                    newObject.SetActive(true);
+                }
+
+            if (hit.transform.tag == "RuleOfThirdsInfo")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+
+            
+
+
+        }
+    }
+}
